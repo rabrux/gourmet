c83e7ad603a8eebeac6cab1bcb6f1fdc4525298c
rsalvador <rsalvador@wat.mx>
2015-04-20 21:01:12 -0500

    menu scrapping ended

--- get.php
+++ get.php
@@ -14,67 +14,31 @@ $html = str_get_html(httpGet('http://www.elbuengourmet.mx/menu-de-hoy.php'));
 $array = array();
 
 foreach ($html->find('table tr') as $e) {
+	$tiempo = false;
 	if (strpos($e->innertext, '<h3>') !== false) {
 		$header = preg_replace(array('/\&nbsp;/', '/\$/'), array('', ''), $e->innertext);
 		$header = trim( strip_tags( $header ) );
 		$array[$header] = array();
+		$platillos = array();
 	} else {
 		$flag = true;
+		$platillo = array();
 		foreach ($e->find('td') as $f) {
-			$platillo = array();
 			if ($flag) {
-				var_dump('name');
-				// $platillo['name'] = trim( strip_tags( $f->innertext ) );
-				// $flag = false;
+				$flag = false;
+				$platillo['name'] = trim( strip_tags( $f->innertext ) );
 			} else {
-				var_dump('price');
-				// $platillo['price'] = trim( strip_tags( $f->innertext ) );
+				$platillo['price'] = trim( strip_tags( $f->innertext ) );
 			}
 		}
-		// var_dump($platillo);
-		// exit;
+		array_push($platillos, $platillo);
 	}
-	// foreach ($e->find('td') as $f) {
-		
-	// }
-	// var_dump(trim( strip_tags( $e->innertext ) ));
-	// exit;
-	// $arr[] = trim( strip_tags( $e->innertext ) );
+
+	$array[$header] = array_merge($platillos, $platillos);
+
 }
-
-var_dump($array); exit;
-
-// foreach($html->find('table tr td') as $e){
-// 	var_dump($e); 
-// 	$arr[] = trim( strip_tags( $e->innertext ) );
-// }
-
-// print_r($arr); exit;
-
-// $html = str_get_html($vartables);   
-// $theData = array();
-
-// foreach($html->find('table') as $onetable){
-// foreach($onetable->find('tr') as $row) {
-
-//     $rowData = array();
-//     foreach($row->find('td') as $cell) {
-//         if(substr_count($cell->innertext,"src")>0){
-//         foreach($cell->find('img') as $element) {
-//         $rowData[] = $element->src;
-//         }
-//         }else{
-//         $rowData[] = $cell->innertext;
-//         }
-//     }
-
-//     $theData[] = $rowData;
-// }
-// }
-
-echo json_encode($theData); exit;
-
-print_r($theData);
+header('Content-Type: application/json', true, 200);
+echo json_encode($array); exit;
 
 function httpGet($url)
 {
@@ -82,7 +46,6 @@ function httpGet($url)
  
     curl_setopt($ch,CURLOPT_URL,$url);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-//  curl_setopt($ch,CURLOPT_HEADER, false); 
  
     $output=curl_exec($ch);
  
